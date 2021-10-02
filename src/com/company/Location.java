9a534a7b18f36e6344eb3aec6d401306a9f7d0cc
Gösta <gosta@MacBook-Pro-som-tillhor-Gosta.local>
2021-10-02 13:27:23 +0200

    New commit before changing the structure of how the Location constructor

--- src/com/company/Location.java
+++ src/com/company/Location.java
@@ -26,53 +26,54 @@ public class Location {
     public String getAddress() {return address;}            // A public function to get the address of the location
     public int getTaxiCost() {return taxiCost;}            // A public function to get the address of the location
 
-     public void locationInput(String[] uberLocation, int currentWalletBalance) {
-            boolean running = true;             //As long as running is true the while loop will be executed
-            try {                   //Try prevents the input to be of an invalid value
-                while (running) {
-                    Scanner input = new Scanner(System.in);  //create a new scanner object
-                    int flag = 0;
-                    //Flag is created to used to determine if the menuChoice is included in the list
+     public String locationInput(String[] uberLocation) {
+         boolean running = true;             //As long as running is true the while loop will be executed
+         try {                   //Try prevents the input to be of an invalid value
+             while (running) {
+                 Scanner input = new Scanner(System.in);  //create a new scanner object
+                 int flag = 0;
+                 //Flag is created to used to determine if the menuChoice is included in the list
 
-                    System.out.println("Where in the city would you like to go?: ");
-                    for (int i = 0; i < uberLocation.length; i++) {
-                        System.out.println((i + 1) + ". " + uberLocation[i]);
-                    }
-                    System.out.print("< ");
-                    String menuChoice = input.nextLine();
+                 System.out.println("Where in the city would you like to go?: ");
+                 for (int i = 0; i < uberLocation.length; i++) {
+                     System.out.println((i + 1) + ". " + uberLocation[i]);
+                 }
+                 System.out.print("< ");
+                 String menuChoice = input.nextLine();
 
-                    //The flag will be set to 1 if there is a match. Also checks if the character still has money
-                    for (int i = 0; i < uberLocation.length; i++ ) {
-                         if ((menuChoice.equalsIgnoreCase(uberLocation[i])) && (currentWalletBalance>taxiCost)) {
-                             flag += 1;
-                             currentLocation = menuChoice;
-                         }
-                    }
+                 //The flag will be set to 1 if there is a match. Also checks if the character still has money
+                 for (int i = 0; i < uberLocation.length; i++) {
+                     if ((menuChoice.equalsIgnoreCase(uberLocation[i]))) {
+                         flag += 1;
+                         currentLocation = menuChoice;
+                     }
+                 }
 
-                    if (menuChoice.length() <= 0 || flag == 0) {
-                        System.out.println("Either does the location not exist or you're out of money");
-                        continue;
+                 if (currentLocation.equalsIgnoreCase("Airport")) {
+                     System.out.println("Airport");
+                     break;
+                 }
+                 if (menuChoice.length() <= 0 || flag == 0) {
+                     System.out.println("Either does the location not exist or you're out of money");
+                     continue;
+                 }
+                 running = false;
+             }
+         } catch (Exception e) {
+             System.out.println("ERROR: Enter a valid value");
+         }
+         return currentLocation;
 
-                    }
-                    running = false;
+     }
 
-                }
-            } catch (Exception e) {
-                System.out.println("ERROR: Enter a valid value");
-            }
-        }
-    public boolean canAffordTaxi(int currentWalletBalance) {
-        boolean canAfford = true;
-        if (currentWalletBalance < taxiCost) {
-            canAfford = false;
-        }
-        return canAfford;
-    }
 
 
     public void goingToMsg(String inLocation) {
-        System.out.println("Going to " + inLocation);
+        System.out.println("Going to " + inLocation + "...........");
+     }
 
-
+     public void welcomeToLocation(String inLocation) {
+         System.out.println("Hi and welcome to " + name);
+         System.out.println(name + " ," + description);
      }
 }
--- src/com/company/Uber.java
+++ src/com/company/Uber.java
@@ -5,39 +5,40 @@ import java.util.Arrays;
 import java.util.List;
 
 public class Uber {
+    String currentUberLocation;
 
     public void gameLoop() {
         Character character = new Character();              //Creating our character object
-        Location startLocation = new Location("The starting point", "We are at the airport" ,
+        Location startLocation = new Location("The starting point", "Hollywood Blv",
                 "This is the main area", 0);
         Beach longBeach = new Beach(24, "Long Beach", "California Avenue",
-                "A white sand beach", 25); ///Creating a beach object
+                "Here you will find a white sand beach", 25); ///Creating a beach object
         Beach shortBeach = new Beach(26, "Short Beach", "Miami Rd",
-                "A beach with lots of surfers", 20); ///Creating another beach object
+                "This is a beach with lots of surfers", 20); ///Creating another beach object
         Casino ceasar = new Casino("Ceasar Palace", "Sunset Drive 70",
-                "Time to gain back some money", 35);
+                "Here you can win back some money", 35);
+        Location airport = new Location("Airport", "Airport Strip",
+                "Here you can leave LA", 0);
 
-        String [] uberLocations = {shortBeach.getName(), longBeach.getName()};
 
-
-        ceasar.playDice();
+        String[] uberLocations = {shortBeach.getName(), longBeach.getName(), ceasar.getName(), airport.getName()};
 
         // A function where inputs needs to be made on the console. This will be saved in character variables
         //character.characterInput();
 
-        // A function where the input can be entered. Input must match on the locations in the uberLocation list
-        //startLocation.locationInput(uberLocations, character.walletBalance);
 
-        //character.updatedWalletBalance(longBeach.getTaxiCost());
+        boolean gameRunning = true; //Variable used to determine if the main game is running
+            while (gameRunning) {
+                // A function where the input can be entered. Input must match on the locations in the uberLocation list
+                currentUberLocation = startLocation.locationInput(uberLocations);
+                startLocation.welcomeToLocation(currentUberLocation);
 
-        //startLocation.goingToMsg(startLocation.currentLocation);
+                gameRunning = false;
+
+            }
 
 
 
 
-        //System.out.println(character.name);
-        //System.out.println(character.age);
-        //System.out.println(character.walletBalance);
-        //System.out.println(character.choiceOfHat);
     }
 }
